e7991dbec3a78bda1b518040d894114dfa7d09bb
pixar0407 <pixar0407@naver.com>
2019-12-08 03:17:13 +0900

    no max pool

--- model.py
+++ model.py
@@ -35,17 +35,17 @@ class convnet(nn.Module):
         #     nn.ReLU(),
         #     nn.MaxPool2d(2)
         # )
-        self.layer4 = nn.Sequential(
+        self.fc1 = nn.Sequential(
             nn.Linear( 14* 14 *16, 1024),
             nn.ReLU()
         )
-        # self.layer5 = nn.Sequential(
-        #     nn.Linear(120, 50),
-        #     nn.ReLU()
-        # )
-        self.layer6 = nn.Sequential(
+        self.fc2 = nn.Sequential(
+            nn.Linear(1024, 120),
+            nn.ReLU()
+        )
+        self.fc3 = nn.Sequential(
             # nn.Dropout(0.3),
-            nn.Linear(1024, 50)
+            nn.Linear(120, 50)
         )
 
     def forward(self, x):
@@ -53,6 +53,6 @@ class convnet(nn.Module):
         x = self.layer2(x)
         # x = self.layer3(x)
         x = x.view(-1, 14*14*16)
-        x = self.layer4(x)
-        # x = self.layer5(x)
-        return self.layer6(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        return self.fc3(x)
