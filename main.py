abf0681075b82e776cdf50833a4788b996cde772
pixar0407 <pixar0407@naver.com>
2019-12-08 01:34:26 +0900

    batch size

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ model = convnet().to(device)
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
 
-num_epochs = 2
+num_epochs = 1
 
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
