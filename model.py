aebdac9a18f201b97b93383d3ab1327335652c11
pixar0407 <pixar0407@naver.com>
2019-12-08 03:23:56 +0900

    no max pool

--- model.py
+++ model.py
@@ -18,41 +18,48 @@ class convnet(nn.Module):
     def __init__(self):
         super().__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(1, 6, 5, stride = 1, padding=1),
+            nn.Conv2d(1, 6, 3, stride = 1, padding=1),
             nn.BatchNorm2d(6),
             nn.ReLU(),
             # nn.MaxPool2d(2)
         )
         self.layer2 = nn.Sequential(
-            nn.Conv2d(6, 16, 5, stride = 1, padding=1),
+            nn.Conv2d(6, 16, 3, stride = 1, padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
-        # self.layer3 = nn.Sequential(
-        #     nn.Conv2d(16, 32, 5, stride = 1, padding = 2),
-        #     nn.BatchNorm2d(32),
-        #     nn.ReLU(),
-        #     nn.MaxPool2d(2)
-        # )
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(16, 32, 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            # nn.MaxPool2d(2)
+        )
+        self.layer4 = nn.Sequential(
+            nn.Conv2d(16, 32, 3, stride = 1, padding = 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
         self.fc1 = nn.Sequential(
-            nn.Linear( 14* 14 *16, 1024),
+            nn.Linear( 16* 16 *32, 2048),
             nn.ReLU()
         )
-        self.fc2 = nn.Sequential(
-            nn.Linear(1024, 120),
-            nn.ReLU()
-        )
+        # self.fc2 = nn.Sequential(
+        #     nn.Linear(120, 50),
+        #     nn.ReLU()
+        # )
         self.fc3 = nn.Sequential(
             # nn.Dropout(0.3),
-            nn.Linear(120, 50)
+            nn.Linear(2048, 50)
         )
 
     def forward(self, x):
         x = self.layer1(x)
         x = self.layer2(x)
-        # x = self.layer3(x)
-        x = x.view(-1, 14*14*16)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = x.view(-1, 16*16*32)
         x = self.fc1(x)
-        x = self.fc2(x)
+        # x = self.fc2(x)
         return self.fc3(x)
