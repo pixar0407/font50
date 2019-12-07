757163f1c389bbcf1d576bfe26359601261acc05
pixar0407 <pixar0407@naver.com>
2019-12-08 04:50:43 +0900

    no max pool

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ else:
 model = convnet().to(device)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.00012)
 
 num_epochs = 1
 
