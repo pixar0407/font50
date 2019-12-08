b2f4dff8678072860546408ac9be6d87bee70fd4
pixar0407 <pixar0407@naver.com>
2019-12-08 13:50:18 +0900

    cnn 단 하나 제거

--- model.py
+++ model.py
@@ -57,7 +57,7 @@ class convnet(nn.Module):
     def forward(self, x):
         x = self.layer1(x)
         x = self.layer2(x)
-        # x = self.layer3(x)
+        x = self.layer3(x)
         # x = self.layer4(x)
         x = x.view(-1, 16*16*32)
         x = self.fc1(x)
