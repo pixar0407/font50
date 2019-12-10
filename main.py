ffd863bd9bc2437d8bd3a84eee0eb34cd5e2023c
pixar0407 <pixar0407@naver.com>
2019-12-10 20:40:34 +0900

    HS

--- main.py
+++ main.py
@@ -26,9 +26,9 @@ model = convnet().to(device)
 
 criterion = nn.CrossEntropyLoss()
 # optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=5e-6)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=7e-6) # 7e-6이 최고 with batch 1 epoch 2
 
-num_epochs = 2
+num_epochs = 4
 for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
         # Assign Tensors to Configured Device
