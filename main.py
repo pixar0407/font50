70083e21f2de923eaedbdf28c26e65780172b9e7
pixar0407 <pixar0407@naver.com>
2019-12-10 20:34:36 +0900

    HS

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ model = convnet().to(device)
 
 criterion = nn.CrossEntropyLoss()
 # optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=7e-6)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=5e-6)
 
 num_epochs = 2
 for epoch in range(num_epochs):
