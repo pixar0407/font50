a9fb35a416a11609a20fde043d64eabe294f6556
pixar0407 <pixar0407@naver.com>
2019-12-10 22:22:59 +0900

    max adap pooling

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ model = convnet().to(device)
 
 criterion = nn.CrossEntropyLoss()
 # optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=1e-5) # 7e-6이 최고 with batch 1 epoch 2
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=7e-6) # 7e-6이 최고 with batch 1 epoch 2
 
 num_epochs = 4
 for epoch in range(num_epochs):
